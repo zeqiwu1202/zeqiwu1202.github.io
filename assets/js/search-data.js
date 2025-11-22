4c3c70557f96c63772a99488a84bcba48cbe3e1e
zeqiwu1202 <wuzeqi@ruc.edu.cn>
2025-11-22 11:34:32 +0000

    Deploying to gh-pages from @ zeqiwu1202/zeqiwu1202.github.io@7d50761a59a692556ab68d5955605723a8773cac 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,6 +16,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
+        },{id: "nav-cv",
+          title: "CV",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/cv/";
+          },
         },{
       id: 'light-theme',
       title: 'Change theme to light',
