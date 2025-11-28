1c7df1d0a9238b015966bda92c44c1b9ef902213
zeqiwu1202 <wuzeqi@ruc.edu.cn>
2025-11-28 07:14:11 +0000

    Deploying to gh-pages from @ zeqiwu1202/zeqiwu1202.github.io@fae3fffb401112749dd0463cca8095b40de73fe2 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,19 +16,19 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
-        },{id: "nav-cv",
-          title: "CV",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/cv/";
-          },
         },{id: "nav-presentations",
           title: "Presentations",
           description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/presentations/";
+          },
+        },{id: "nav-cv",
+          title: "CV",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/cv/";
           },
         },{
         id: 'social-arxiv',
