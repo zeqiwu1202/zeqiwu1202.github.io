b69ebccb14121bca39a22a6bf98d37c391df4469
Zeqi Wu <wuzeqi@ruc.edu.cn>
2026-01-27 17:42:23 +0800

    speedup

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,41 +1,4 @@
 $(document).ready(function () {
-  const root = document.documentElement;
-  const markLoaded = function (key, cls) {
-    try {
-      sessionStorage.setItem(key, "1");
-    } catch (e) {}
-    root.classList.add(cls);
-  };
-
-  const maybeRestore = function (key, cls) {
-    try {
-      if (sessionStorage.getItem(key) === "1") {
-        root.classList.add(cls);
-        return true;
-      }
-    } catch (e) {}
-    return false;
-  };
-
-  if (document.fonts && document.fonts.load) {
-    if (!maybeRestore("fa-loaded", "fa-loaded")) {
-      document.fonts.load('1em "Font Awesome 6 Free"').then(function () {
-        markLoaded("fa-loaded", "fa-loaded");
-      });
-      document.fonts.load('1em "Font Awesome 6 Brands"').then(function () {
-        markLoaded("fa-loaded", "fa-loaded");
-      });
-    }
-
-    if (!maybeRestore("ai-loaded", "ai-loaded")) {
-      document.fonts.load('1em "Academicons"').then(function () {
-        markLoaded("ai-loaded", "ai-loaded");
-      });
-    }
-  } else {
-    root.classList.add("fa-loaded", "ai-loaded");
-  }
-
   // add toggle functionality to abstract, award and bibtex buttons
   $("a.abstract").click(function () {
     $(this).parent().parent().find(".abstract.hidden").toggleClass("open");
